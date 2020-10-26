d7119141c1546590d02c668af529000f472d8225
Tonatiuh Núñez <tonatiuh.nunez@ooma.com>
2020-10-26 06:16:58 -0600

    change UI so that it shows information from messages from Kafka https://share.getcloudapp.com/Jrug898P

--- ui/public/js/main.js
+++ ui/public/js/main.js
@@ -4,6 +4,9 @@ window.onload = function() {
   let $chatbox = document.querySelector('#chatbox');
 
   socket.on('connect', connection => {
+    var $localStudentId = document.getElementById('local-student-id');
+    $localStudentId.innerText = `Student ID: ${socket.id}`;
+
     $chatbox.addEventListener('keyup', event => {
       socket.emit('event', {
         current: event.key,
@@ -12,6 +15,30 @@ window.onload = function() {
     });
 
     socket.on('action', action => {
+      if (socket.id === action.metadata.socketId) return;
+
+      const $otherStudentsWork = document.getElementById('other-students-work');
+      var $studentWork = document.querySelectorAll(`#other-students-work #${action.metadata.socketId}`);
+
+      if ($studentWork.length) {
+        var $textArea = document.querySelectorAll(`#other-students-work #${action.metadata.socketId} textarea`)[0];
+      }
+      else {
+        var $studentWork = document.createElement('div');
+        $studentWork.setAttribute('id', action.metadata.socketId);
+        $otherStudentsWork.appendChild($studentWork);
+
+        var $title = document.createElement('h2');
+        $title.setAttribute('class', 'title');
+        $title.innerText = `Student ID: ${action.metadata.socketId}`;
+        $studentWork.appendChild($title);
+
+        var $textArea = document.createElement('textarea');
+        $textArea.setAttribute('disabled', 'disabled');
+        $studentWork.appendChild($textArea);
+      }
+
+      $textArea.value = action.payload.content;
       console.log(action);
     });
   });
